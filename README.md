fb3a89fe745139f3e403268bf4f914a1eac0a07e
Noan <noan.pujol@gmail.com>
2026-04-21 17:14:19 +0200

    progressign a bit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 trouver de la doc en ligne
+- faire un bon readme qui explique bien
+- whats a derivation and why do we use it on the MSE
--- main.py
+++ main.py
@@ -4,14 +4,35 @@ import pandas as pd
 
 def estimatedPrice(theta_0, theta_1 ,mileage):
     return theta_0 + (theta_1 * mileage)
-    
+
+
+def calculate_sum__of_error(theta0, theta1, i, data : pd.DataFrame):
+    index = 1
+    summ = 0
+    for row in data.itertuples(index=False):
+        if i <= index:
+            return summ
+        summ += (estimatedPrice(theta0, theta1, row.km) - row.price)
+        index+=1
+    return summ
+
+
 def train_on_data(data : pd.DataFrame):
     # print(data)
     theta_0 = 0
     theta_1 = 0
-    for row in data.itertuples(index=False):
-        theta_1 = estimatedPrice(theta_0, theta_1, row.price) * row.price
-    print(theta_1)
+    learning_rate = 0.01
+    i = 1
+    for _ in range(100):
+        i = 1
+        for row in data.itertuples(index=False):
+            sum_of_error = calculate_sum__of_error(theta_0, theta_1, i, data)
+            final_error = sum_of_error * (1 / i)
+            theta_0 = theta_0 - (learning_rate * final_error)
+            theta_1 = theta_1 - (learning_rate * final_error)
+            i+=1
+            print("here",theta_1)
+    print(estimatedPrice(theta_0, theta_1, 500))
 
 def main():
     argv = sys.argv
